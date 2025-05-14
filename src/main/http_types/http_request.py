8e7f48fc0c8658af36baac8966e7f5f3dd1c1160
taleshmm <taleshmm@hotmail.com>
2025-05-14 18:08:17 -0300

    feat: implementing registry_finder

--- src/main/http_types/http_request.py
+++ src/main/http_types/http_request.py
@@ -1,5 +1,10 @@
 class HttpRequest:
-    def __init__(self, body: dict = None, headers: dict = None, params: dict = None) -> None:
+    def __init__(
+        self, body: dict = None, 
+        headers: dict = None, 
+        path_params: dict = None, 
+        query: dict = None) -> None:
         self.body = body
         self.headers = headers
-        self.params = params
+        self.path_params = path_params
+        self.query = query
