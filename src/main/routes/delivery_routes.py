64241eb81239503ea65be4b8a9b0333318589b99
taleshmm <taleshmm@hotmail.com>
2025-05-14 15:35:32 -0300

    feat: Addin use case in registry order routes

--- src/main/routes/delivery_routes.py
+++ src/main/routes/delivery_routes.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, jsonify, request
 from src.main.http_types.http_request import HttpRequest
-from src.main.http_types.http_response import HttpResponse
+from src.main.composer.registry_order_composer import registry_order_composer
 
 delivery_routes_bp = Blueprint('delivery_routes', __name__)
 
 @delivery_routes_bp.route('/delivery/order', methods=['POST'])
 def registry_order():
+    user_case = registry_order_composer()
     http_request = HttpRequest(body=request.json)
-    print(request.json)
-    return jsonify({"message": "Order registered successfully"}), 201
+    response = user_case.registry(http_request)
+    
+    return jsonify(response.body), response.status_code
