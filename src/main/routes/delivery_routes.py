3647984c43d8b2d7c529c7ac7ec7a0c8ac33cb2d
taleshmm <taleshmm@hotmail.com>
2025-05-14 14:21:27 -0300

    feat: Implementing server

---
+++ src/main/routes/delivery_routes.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, jsonify, request
+delivery_routes_bp = Blueprint('delivery_routes', __name__)
+
+@delivery_routes_bp.route('/delivery/order', methods=['POST'])
+def registry_order():
+    print(request.json)
+    return jsonify({"message": "Order registered successfully"}), 201
--- src/models/repository/orders_repository.py
+++ src/models/repository/orders_repository.py
@@ -6,7 +6,6 @@ class OrdersRepository(OrdersRepositoryInterface):
         self.__collection_name = "orders"
         self.__db_connection = db_connection
         
-    
     def insert_document(self, document: dict) -> None:
         collection = self.__db_connection.get_collection(self.__collection_name)
         collection.insert_one(document)
